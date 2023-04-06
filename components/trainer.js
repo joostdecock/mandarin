266532a8f5e6afa1b45a5b15167aede9df8737a8
joostdecock <joost@joost.at>
2023-04-06 10:48:08 +0200

    feat: Added loading indicator

--- components/trainer.js
+++ components/trainer.js
@@ -6,6 +6,7 @@ import { asSlug } from 'scripts/utils.mjs'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import PlayButton from 'components/audio.mjs'
+import { Spinner } from 'components/spinner.mjs'
 
 const TranslationButton = ({ target, word }) => (
   <Link href={asSlug(word[target])}>
@@ -36,6 +37,7 @@ const WordTrainer = ({
 }) => {
 
   const router = useRouter()
+  const [ loading, setLoading ] = useState(false)
   const [ show, setShow ] = useState(false)
   const [ next, setNext ] = useState(false)
   const [ upcoming, setUpcoming ] = useState([])
@@ -52,9 +54,12 @@ const WordTrainer = ({
       }
     }
     getReady()
+    setLoading(false)
   }, [ slug ])
 
+  // Swipe left triggers this
   const nextWord = () => {
+    setLoading(true)
     if (show) setShow(false)
     const [next, prefix] = upcoming[0] //getNext(slug)
     setUpcoming(upcoming.slice(1))
@@ -64,7 +69,16 @@ const WordTrainer = ({
     router.push(prefix+asSlug(to))
   }
 
+  // Swipe right triggers this
   const revealMemo = () => setShow(true)
+
+  // Swipe down triggers this
+  const showLessOften = () => {
+  }
+  // Swipe up triggers this
+  const showMoreOften = () => {
+  }
+
   const modes = { en, cn, py }
   const app = useApp()
 
@@ -78,21 +92,27 @@ const WordTrainer = ({
     >
       <div className="max-w-xl h-screen flex flex-col items-center justify-center px-4 gap-4 m-auto">
         <h1 className="text-center text-7xl break-all">
-          <PlayButton word={cn} autoPlay={app.settings?.autoPlay} slug={slug}>
-            {type === 'cn' && <span className="block text-4xl opacity-50">{[...''+tone].map(ch => ch).join(' ')}</span>}
-            {modes[type]}
-            {app.settings?.cnpy && type === 'cn' && <span className="block text-4xl opacity-50 pt-2">{py}</span>}
-            {app.settings?.cnpy && type === 'py' && <span className="block text-4xl opacity-50 pt-2">{cn}</span>}
-          </PlayButton>
-        {show && (
-          <div className="flex flex-row gap-8 items-center mt-2 justify-center">
-            {type !== 'cn' && <Link className="block text-4xl text-secondary underline" href={cn+tone}>{cn}</Link>}
-            {type !== 'py' && <Link className="block text-4xl text-secondary underline" href={asSlug(py)}>{py}</Link>}
-            {type !== 'en' && <Link className="block text-4xl text-secondary underline" href={asSlug(en)}>{en}</Link>}
-          </div>
-        )}
+          {loading
+            ? <Spinner className="w-24 h-24" />
+            : (
+              <>
+                <PlayButton word={cn} autoPlay={app.settings?.autoPlay} slug={slug}>
+                  {type === 'cn' && <span className="block text-4xl opacity-50">{[...''+tone].map(ch => ch).join(' ')}</span>}
+                  {modes[type]}
+                  {app.settings?.cnpy && type === 'cn' && <span className="block text-4xl opacity-50 pt-2">{py}</span>}
+                  {app.settings?.cnpy && type === 'py' && <span className="block text-4xl opacity-50 pt-2">{cn}</span>}
+                </PlayButton>
+                {show && (
+                  <div className="flex flex-row gap-8 items-center mt-2 justify-center">
+                    {type !== 'cn' && <Link className="block text-4xl text-secondary underline" href={cn+tone}>{cn}</Link>}
+                    {type !== 'py' && <Link className="block text-4xl text-secondary underline" href={asSlug(py)}>{py}</Link>}
+                    {type !== 'en' && <Link className="block text-4xl text-secondary underline" href={asSlug(en)}>{en}</Link>}
+                  </div>
+                )}
+              </>
+            )}
         </h1>
-        {show && (
+        {show && !loading && (
           <div className="text-lg text-center mdx">
             {memo.split("\n").map((line, i) => <span key={i} className="block" dangerouslySetInnerHTML={{__html: line}} />)}
             {also && (
@@ -105,15 +125,19 @@ const WordTrainer = ({
             )}
           </div>
         )}
-        <button 
-          onClick={() => setShow(!show)}
-          className="btn btn-secondary btn-link hidden lg:block"
-        >{show ? 'Hide' : 'Show'} memo
-        </button>
-        <button 
-          className="btn btn-secondary btn-wide btn-lg mt-8 lg:block btn-outline border-2 hidden"
-          onClick={nextWord}
-        >next</button>
+        {!loading && (
+          <>
+            <button 
+              onClick={() => setShow(!show)}
+              className="btn btn-secondary btn-link hidden lg:block"
+            >{show ? 'Hide' : 'Show'} memo
+            </button>
+            <button 
+              className="btn btn-secondary btn-wide btn-lg mt-8 lg:block btn-outline border-2 hidden"
+              onClick={nextWord}
+            >next</button>
+          </>
+        )}
       </div> 
     </Page>
   )
